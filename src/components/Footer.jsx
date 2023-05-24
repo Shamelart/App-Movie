87c21afa5dc7e97442e881d1283303aec3836242
Shamelart <shamelart.ca@gmail.com>
2023-05-23 21:43:58 -0300

    modificacion footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	return (
 		<Container style={{ backgroundColor: "black", maxWidth: "100%", height:"70px", marginTop:"110px"}} >
 			<Typography variant="h5" sx={{color: "red", textAlign: "center", paddingTop: "20px"}}>
-				Hecho por Shamelart
+				Hecho por  Shamelart...
 			</Typography>
 		</Container>
 
